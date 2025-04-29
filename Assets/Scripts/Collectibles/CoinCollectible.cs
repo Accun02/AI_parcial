998dbfb7fa959cc2d6686a2b1ce40657413cb85a
solangebordeta <solangebordeta@live.com>
2025-04-29 15:15:30 -0300

    cambios:
    - canvas para el collector de monedas
    - nuevo script coinUI

--- Assets/Scripts/Collectibles/CoinCollectible.cs
+++ Assets/Scripts/Collectibles/CoinCollectible.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinCollectible : MonoBehaviour
 {
     public int numberOfCoins {  get; private set; } //cualquier script puede leer el valor
-                                                     //pero solo este setea el valor
+                                                    //pero solo este setea el valor
+    
+
+    public UnityEvent<CoinCollectible> OnCoinsCollected;
 
     public void CoinsCollected()
     {
         numberOfCoins++; //aumenta el numero de monedas cuando son agarradas
+        OnCoinsCollected.Invoke(this); 
     }
+
+    
 
 }
---
+++ Assets/Scripts/Collectibles/CoinUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinUI : MonoBehaviour
+{
+    private CoinCollectible coinCollectible;
+    private TextMeshProUGUI coinText;
+    private int totalCoins = 7;
+
+
+    void Start()
+    {
+        coinText = GetComponent<TextMeshProUGUI>();
+        UpdateCoinText(coinCollectible);
+    }
+
+    public void UpdateCoinText(CoinCollectible coinCollectible)
+    {
+        coinText.text = coinCollectible.numberOfCoins.ToString();
+        coinText.text = coinCollectible.numberOfCoins + "/" + totalCoins; //muestra en el canvas monedas agarradas / total
+    }
+
+}
