65f74557048045eb9bb64d27876c8a5bc6f20f75
eli094 <elizabethiscool094@gmail.com>
2025-07-08 02:54:42 -0300

    MinionScenePlayerControllerChanged

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
         {
             if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 100, eneemymask) && ammo > 0)
             {
-                hit.collider.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+                hit.collider.gameObject.GetComponentInParent<Enemy>().TakeDamage(damage);
             }
             ammo--;
         }
